70a72f795600ee80e4af1e855b1be98a484c8b25
Richard <richard.yang.tong@gmail.com>
2021-07-14 11:10:17 -0400

    Added notes.

---
+++ HelloFriend/src/test/java/com/dragontalker/maven/HelloFriendTest.java
@@ -0,0 +1,13 @@
+package com.dragontalker.maven;
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+import com.dragontalker.maven.Hello;
+
+public class HelloFriendTest {
+    @Test
+    public void testHelloFriend() {
+        HelloFriend helloFriend = new HelloFriend();
+        String results = helloFriend.sayHelloToFriend("litingwei");
+        assertEquals("Hello litingwei! I am John", results);
+    }
+}
