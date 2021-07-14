0aee63159b9697f28d1fe6ba6aeea7c23df44311
Richard <richard.yang.tong@gmail.com>
2021-07-14 14:52:23 -0400

    Added notes.

--- MakeFriends/src/main/java/com/dragontalker/maven/MakeFriends.java
+++ MakeFriends/src/main/java/com/dragontalker/maven/MakeFriends.java
@@ -2,4 +2,8 @@ package com.dragontalker.maven;
 
 public class MakeFriends {
 
+	public String makeFriends(String name) {
+		HelloFriend friend = new HelloFriend();
+		friend.sayHelloToFriend("litingwei");
+	}
 }
