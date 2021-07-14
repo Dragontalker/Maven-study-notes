9566ff122112848ba7ab146c3efc995699098a56
Richard <richard.yang.tong@gmail.com>
2021-07-14 11:10:47 -0400

    Added notes.

--- HelloFriend/src/main/java/com/dragontalker/maven/HelloFriend.java
+++ HelloFriend/src/main/java/com/dragontalker/maven/HelloFriend.java
@@ -1,7 +1,7 @@
 package com.dragontalker.maven;
 import com.dragontalker.maven.Hello;
 public class HelloFriend {
-    public String sayHelloFriend(String name) {
+    public String sayHelloToFriend(String name) {
         Hello hello = new Hello();
         String str = hello.sayHello(name) + " I am " + this.getMyName();
         System.out.println(str);
